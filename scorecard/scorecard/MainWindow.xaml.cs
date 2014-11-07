d29f73aedc890244610a179cedfc6c983691f480
moeru <moeru.ookami@gmail.com>
2014-11-06 23:17:20 -0800

    Created basic model objects, added file menu

--- scorecard/scorecard/MainWindow.xaml.cs
+++ scorecard/scorecard/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using scorecard.Model;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,40 @@ namespace scorecard
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SessionModel _sessionModel;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void NewFile(object sender, RoutedEventArgs e)
+        {
+            _sessionModel = new SessionModel();
+        }
+
+        private void SaveFile(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.FileName = "Results";
+            dialog.DefaultExt = ".json";
+            dialog.Filter = "JSON Documents (.json)|*.json";
+
+            var result = dialog.ShowDialog();
+
+            if (result.HasValue && result.Value)
+            {
+                using (var streamWriter = new StreamWriter(new FileStream(dialog.FileName, FileMode.Create)))
+                {
+                    streamWriter.Write(JsonConvert.SerializeObject(_sessionModel, Formatting.Indented));
+                }
+            }
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
     }
 }
